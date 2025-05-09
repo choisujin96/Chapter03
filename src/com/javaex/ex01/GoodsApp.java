694b1c565ac76dfa0a03ff2d16351d91af3d045c
choisujin96 <csjin9783@naver.com>
2025-05-09 17:54:07 +0900

    2025-05-09 *object class & equals*

---
+++ src/com/javaex/ex01/GoodsApp.java
@@ -0,0 +1,25 @@
+package com.javaex.ex01;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+
+		//모든 클래스의 부모는 Object 클래스 이다.
+		Goods camera = new Goods();
+		camera.hashCode(); //내가 만든 메소드가 아님.
+						  //부모 object 클래스의 메소드
+		
+		
+		Object obj = new Object();
+		System.out.println(obj.getClass());   //결과:class java.lang.Object  --> 클래스
+		System.out.println(obj.toString());   //결과:java.lang.Object@28a418fc  --> 출력정보
+		System.out.println(obj.hashCode());  //결과: 681842940               --> 당분간 주소라고 생각하자 0x333     
+		System.out.println(obj.equals(camera)); //결과: false				--> 두개가 같냐?
+		
+		
+		
+		
+		
+	}
+
+}
